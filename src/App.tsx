d831195c3be86e03c2b7509d08d516671aca3d27
Axi <axidreamdragon@gmail.com>
2025-07-09 17:48:15 +0200

    Implement Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
 import JustDrawing from "./JustDrawing/JustDrawing";
 
 function App() {
   return (
-    <JustDrawing />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<JustDrawing />} />
+        <Route path="/justdrawing" element={<JustDrawing />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
