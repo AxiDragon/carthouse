c0cf8c4ec12c5b1cd304343da4f20b7285b99a4e
Axi <axidreamdragon@gmail.com>
2025-07-10 10:09:21 +0200

    Implement auto route creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
-import JustDrawing from "./JustDrawing/JustDrawing";
-import LimitedInk from "./LimitedInk/LimitedInk";
+import { lazy } from "react";
+
+const modules = import.meta.glob("./*/index.tsx", { eager: true });
+
+const routes = Object.entries(modules).map(([path, module]) => {
+  const routeName = path.split('/')[1].toLowerCase();
+  const Component = lazy(() =>
+    Promise.resolve(module).then((mod: any) => ({
+      default: mod.default,
+    }))
+  );
+
+  return {
+    path: `/${routeName}`,
+    component: Component,
+  };
+});
+
+routes.unshift({ path: '/', component: routes[0]?.component });
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<JustDrawing />} />
-        <Route path="/justdrawing" element={<JustDrawing />} />
-        <Route path="/limitedink" element={<LimitedInk />} />
+        {routes.map(({ path, component: Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
       </Routes>
     </HashRouter>
   )
