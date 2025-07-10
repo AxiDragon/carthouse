27d4925fd2ddd9e1647701fcc07c51886415a98e
Axi <axidreamdragon@gmail.com>
2025-07-10 11:53:42 +0200

    Create basic soft mode

---
+++ src/Soft/index.tsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import ScalingCanvas, { type ScalingCanvasRef } from "../components/ScalingCanvas";
+
+function Soft() {
+	const scalingCanvas = useRef<ScalingCanvasRef>(null);
+
+	const onMouseDown = (e: MouseEvent) => {
+		const canvas = scalingCanvas.current!.getCanvas();
+		const ctx = canvas.getContext('2d');
+		if (!ctx) return;
+
+		ctx.shadowBlur = 25;
+		ctx.shadowColor = 'black';
+	}
+
+	const init = (canvas: HTMLCanvasElement) => {
+		canvas.addEventListener('mousedown', onMouseDown);
+
+		const ctx = canvas.getContext('2d');
+		if (!ctx) return;
+
+		ctx.lineWidth = 1;
+		ctx.strokeStyle = 'black';
+	}
+
+	return (
+		<ScalingCanvas init={init} ref={scalingCanvas} />
+	)
+}
+
+export default Soft;
